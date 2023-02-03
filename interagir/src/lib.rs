4d7561ddab20a0658f8c4113e3a8a9a5a4f77861
felipeozias <95044200+felipeozias@users.noreply.github.com>
2023-02-02 21:40:09 -0300

    feat: new lib

---
+++ interagir/src/lib.rs
@@ -0,0 +1,9 @@
+pub fn escrever(s:&str) {
+    println!("{}", s);
+}
+
+pub fn escrever_bonito(s:&str) {
+    println!("========================================");
+    println!("🟢 {} 🟢", s);
+    println!("========================================");
+}
